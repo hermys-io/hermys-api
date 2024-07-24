437868cc065d52d3cabdd58e8af3d68f22d0830b
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2024-07-24 19:54:22 -0300

    feat: add knowledge train

--- hermys/modules/knowledge/routes.py
+++ hermys/modules/knowledge/routes.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
+from hermys.db.base import ObjectIdField
 from hermys.modules.auth.dependencies import GetCurrentUser
 from hermys.modules.knowledge.dependencies import GetKnowledgeService
 from hermys.modules.knowledge.schemas import KnowledgeCreatePayload
+from hermys.modules.knowledge.services.train_knowledge import (
+    GetTrainKnowledgeService,
+)
 
 router = APIRouter()
 
@@ -15,3 +19,17 @@ async def create_knowledge(
 ):
     result = await knowledge_service.create(payload=payload)
     return result.model_dump()
+
+
+@router.post('/{knowledge_id}/train')
+async def train_knowledge(
+    knowledge_id: ObjectIdField,
+    train_knowledge_service: GetTrainKnowledgeService,
+    current_user: GetCurrentUser,
+):
+    await train_knowledge_service.dispatch(
+        knowledge_id=str(knowledge_id),
+        index_name=current_user.organization,
+    )
+
+    return None
