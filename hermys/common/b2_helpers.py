274ddeb7e49635a7af7ecadec00421dbcdb375bc
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2024-08-27 21:23:41 -0300

    test: test b2_helpers

--- hermys/common/b2_helpers.py
+++ hermys/common/b2_helpers.py
@@ -1,7 +1,5 @@
 from typing import Literal
 
-from hermys.integrations.b2.integration import B2Integration
-
 
 def get_clerk_filename(
     *,
@@ -28,14 +26,3 @@ def get_knowledge_filename(
     file_extension = photo_name.split('.')[-1]
 
     return f'{path}/{file_name}.{file_extension}'
-
-
-def get_signed_url(*, filename: str, valid_duration_in_seconds: int = 3600):
-    b2_integration = B2Integration()
-
-    result = b2_integration.get_file(
-        filename=filename,
-        valid_duration_in_seconds=valid_duration_in_seconds,
-    )
-
-    return result
